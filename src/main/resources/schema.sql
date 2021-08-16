6119b475aae4b91cc6a995dc0b9583df5c8d26c8
piyushbhardwaj <piyush.2018mnnit@gmail.com>
2021-08-16 14:25:12 +0530

    badge schema change

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,18 +1,18 @@
 CREATE TABLE badge (
     id VARCHAR(255) NOT NULL,
-    name VARCHAR (30),
-    description VARCHAR(200),
+    name VARCHAR (225),
+    description VARCHAR(255),
     cost int,
     location VARCHAR (300),
     `level` VARCHAR (30),
-    purchases int
+    purchases int,
     PRIMARY KEY (id)
 );
 
 
 CREATE TABLE user (
     username VARCHAR (255) NOT NULL,
-    email VARCHAR (30),
+    email VARCHAR (225),
     is_active bit(1),
     kudos_point int,
     name VARCHAR (300),
@@ -21,7 +21,7 @@ CREATE TABLE user (
 
 CREATE TABLE user_badges (
     badge_id VARCHAR (255) NOT NULL,
-    username VARCHAR (30),
+    username VARCHAR (255),
     purchased_at date,
     status VARCHAR (30),
     PRIMARY KEY (username, badge_id)
